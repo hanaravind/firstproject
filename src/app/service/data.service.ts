23b1d0681d3e87d1e67f81f0507b9b9004a7b445
Aravindhan <hanaravind5@gmail.com>
2019-05-23 22:53:58 +0530

    add service

---
+++ src/app/service/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+api:string;
+
+  constructor(private http: HttpClient) { 
+    this.api = "https://jsonplaceholder.typicode.com/";
+  }
+
+  public getHttp(key): Observable<any>{
+    return this.http.get(this.api + key);
+  }
+
+  public deleteHttp(key): Observable<any>{
+    return this.http.delete(this.api + key);
+  }
+}
