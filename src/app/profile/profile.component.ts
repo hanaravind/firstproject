f7b5632bf3649cddaf89be1ee270887c74bc2332
DHAARUN RAJA A G <dharun57@gmail.com>
2019-05-25 20:24:16 +0530

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { ItemService } from './../service/item.service';
 import { DataService } from './../service/data.service';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router'; // import the router for getting the value
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -8,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   productData:any =[];
-  constructor(private http:DataService,private itemservice:ItemService) { }
+//   constructor(private http:DataService,private itemservice:ItemService) { }
+  displayString;
+
+  constructor(private http: DataService, private itemservice: ItemService, private route: ActivatedRoute,
+              private router: Router) {
+           console.log('log ', this.route.paramMap['destination'].value.id);
+           this.displayString = this.route.paramMap['destination'].value.id;
+  }
+  
   ngOnInit() {
     } 
 
